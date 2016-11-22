42a14d5bf0aaa910476d91102aea9eaf793aa822
Hugo Vieira <hugo.andre.vieira@gmail.com>
2016-11-22 12:17:58 +0000

    Changed script names and project settings due to Unity Update. Preparing for massive codebase update

---
+++ Assets/Scripts/EndlessModeManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class EndlessModeManager : MonoBehaviour
+{
+	private AudioSource _endlessModeAudio;
+	private bool _gameOverPlaying;
+
+	void Awake()
+	{
+		_gameOverPlaying = false;
+
+		// Endless mode audio
+		_endlessModeAudio = GetComponent<AudioSource>();
+		_endlessModeAudio.clip = Resources.Load("Music/EndlessMode") as AudioClip;
+		_endlessModeAudio.loop = true;
+		_endlessModeAudio.pitch = 1.1f;
+		_endlessModeAudio.Play();
+	}
+
+
+	void Update()
+	{
+		if (PlayerController.GameOver && !_gameOverPlaying)
+		{
+			// Play game over music
+			_endlessModeAudio.clip = Resources.Load("Music/GameOver") as AudioClip;
+			_endlessModeAudio.loop = true;
+			_endlessModeAudio.pitch = 1.0f;
+			_endlessModeAudio.Play();
+			_gameOverPlaying = true;
+		}
+	}
+}
