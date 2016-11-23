91f77700a8e4803c878ad33c3c6edda6d4237f69
Hugo Vieira <hugo.andre.vieira@gmail.com>
2016-11-23 02:31:40 +0000

    Added music to level 1

---
+++ Assets/Scripts/LevelModeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class LevelModeManager : MonoBehaviour
+{
+	private AudioSource _endlessModeAudio;
+	private PlayerController _playerRef;
+	private bool _gameOverPlaying;
+
+	void Awake()
+	{
+		_playerRef = GameObject.Find("Player").gameObject.GetComponent<PlayerController>();
+		_gameOverPlaying = false;
+
+		// Endless mode audio
+		_endlessModeAudio = GetComponent<AudioSource>();
+		_endlessModeAudio.clip = Resources.Load("Music/Level1") as AudioClip;
+		_endlessModeAudio.loop = true;
+		_endlessModeAudio.pitch = 1.1f;
+		_endlessModeAudio.Play();
+	}
+
+
+	void Update()
+	{
+		if (_playerRef.GameOver && !_gameOverPlaying)
+		{
+			// Play game over music
+			_endlessModeAudio.clip = Resources.Load("Music/GameOver") as AudioClip;
+			_endlessModeAudio.loop = true;
+			_endlessModeAudio.pitch = 1.0f;
+			_endlessModeAudio.Play();
+			_gameOverPlaying = true;
+		}
+	}
+}
