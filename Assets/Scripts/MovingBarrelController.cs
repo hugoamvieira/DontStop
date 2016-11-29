9f3a216386e7b57a17d3d75ce196a1106191ebd6
Hugo Vieira <hugo.andre.vieira@gmail.com>
2016-11-29 19:27:20 +0000

    Added new obstacles including a moving barrel!

---
+++ Assets/Scripts/MovingBarrelController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MovingBarrelController : MonoBehaviour
+{
+	private Rigidbody2D _barrelRigidbody;
+	[SerializeField] private float _barrelTorque;
+
+	void Awake()
+	{
+		_barrelRigidbody = gameObject.GetComponent<Rigidbody2D>();
+		_barrelRigidbody.AddTorque(_barrelTorque);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+}
